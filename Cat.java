1714b7a85779f1cb44b72d0fd0c21799c1cd15ae
Your Name <your.email@example.com>
2026-02-20 14:11:52 +0200

    Add `Cat.java` class with constructor and main method example

---
+++ Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    public Cat() {
+        // instructions for creating a Cat instance
+    }
+
+    public static void main(String[] args) {
+        // garfield is declared with reference data type `Cat`
+        Cat garfield = new Cat();
+        System.out.println(garfield); // Prints: Cat@76ed5528
+    }
+}
