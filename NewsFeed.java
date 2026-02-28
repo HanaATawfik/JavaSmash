83aef0610c51a8397cfdff2b15fff26f2e71b6d1
Your Name <your.email@example.com>
2026-02-28 22:09:03 +0200

    Update `NewsFeed.java` class to include `Main` method with example usage of `getTopics` method

--- NewsFeed.java
+++ NewsFeed.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
+
 public class Newsfeed {
     public Newsfeed(){
 
     }
-    // Create getTopics() below:
-    public String[] getTopics()
-    {
-        String[] topics={"Opinion", "Tech", "Science", "Health"};
+
+    public String[] getTopics(){
+        String[] topics = {"Opinion", "Tech", "Science", "Health"};
         return topics;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Newsfeed sampleFeed = new Newsfeed();
+        String[] topics = sampleFeed.getTopics();
+        System.out.println(Arrays.toString(topics));
     }
 }
